857ef7362bde9266ccb597ccbf1158e8be714e24
Sedanur Savaş <44877350+sedsax@users.noreply.github.com>
2021-12-17 21:55:17 +0300

    Update Ödev5Soru2.cpp

--- Ödev5Soru2.cpp
+++ Ödev5Soru2.cpp
@@ -1,4 +1,15 @@
-// Example program
+/*
+Sizden ikisi de aynı zamanda sıfıra eşit olmayan 2 negatif olmayan tam sayının ortak bölenlerinin en 
+büyüğünü (OBEB) Eucild algoritmasını modifiye ederek bulmanız istenmektedir. Bunun için aşağıdaki 
+özellikleri kullanmanız gerekmektedir.
+• 1. Kural: a ve b çift ise OBEB(2a, 2b)=2.OBEB(a,b)
+• 2. Kural: a çift b tek ise OBEB(2a,b)=OBEB(a,b)
+• 3. Kural: OBEB(a,0)=a
+• 4. Kural: a≥b ise OBEB(a,b)=OBEB(b, a-b)
+• 5. Kural: OBEB(a, b)=OBEB(b, a)
+Kodunuzda bölme işlemi olarak sadece 2 ile bölmeye izin verilmektedir.
+*/
+
 #include <iostream>
 #include <string>
 using namespace std;
